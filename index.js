feedbee1f73762b4610e9ba42e5d2cb183aea4b7
kcomain <cheunghowui@gmail.com>
2020-08-12 02:01:57 -0400

    move stuff to app.js because im dumb and didnt know it doesnt work

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-require('./src/eutils');
-const e = require('express');
+require('./src/app');
 
-const app = e();
-
-app.use(e.static('site/'));
-
-let port
-if (process.env.PORT === undefined){
-    port = 8080
-} else if (Number.isNaN(Number(process.env.PORT))){
-    throw new TypeError('PORT has to be a number.')
-} else {
-    port = Number(process.env.port)
-}
-
-
-app.listen(port, () => {
-    console.log(`Started listening to port ${port}`);
-});
+console.log('bruh')
--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ const e = require('express')
 
 const app = e()
 
+app.use(e.static('site/'));
+
+let port
+if (process.env.PORT === undefined) {
+    port = 8080
+} else if (Number.isNaN(Number(process.env.PORT))) {
+    throw new TypeError('PORT has to be a number.')
+} else {
+    port = Number(process.env.port)
+}
+
+
+app.listen(port, () => {
+    console.log(`Started listening to port ${port}`);
+});
+
 app.all('/admin/queries', (req, res) => {
     let resp = "<h1><em><u>URL Query strings</u></em></h1><hr>";
     console.log(req.query);
     console.log(`query entries: ${Object.entries(req.query)}`)
-    for (const [key, value] in Object.entries(req.query)){
+    for (const [key, value] of Object.entries(req.query)) {
         resp += `<p><b>${key}</b>: <code>${String(value)}</code></p>`;
         console.log(`key: ${key}, entry: ${value}`)
     }
